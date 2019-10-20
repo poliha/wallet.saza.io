24716a952a62c90fd87a4c7523168b569aa96e61
oliha <poliha2002@gmail.com>
2019-10-20 14:37:13 -0400

    add notification service

---
+++ src/app/providers/notification.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private toastCtrl: ToastController) { }
+
+  // to do add methods with more options
+  async show(message: string) {
+    if (message === '') {
+      return;
+    }
+
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+}
--- src/app/providers/saza-error-handler.service.ts
+++ src/app/providers/saza-error-handler.service.ts
@@ -1,12 +1,14 @@
 import { Injectable, ErrorHandler } from '@angular/core';
+import { NotificationService } from './notification.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SazaErrorHandler implements ErrorHandler {
-  constructor() { }
+  constructor(private notification: NotificationService) { }
 
   handleError(error: any): void {
     console.error('SEH: ', error);
+    this.notification.show(error);
   }
 }
