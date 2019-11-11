fe27721aefeec6d557be350b82023c524e8d3047
oliha <poliha2002@gmail.com>
2019-11-10 22:47:56 -0500

    offer component

---
+++ src/app/components/offer/offer.component.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OfferComponent } from './offer.component';
+
+describe('OfferComponent', () => {
+  let component: OfferComponent;
+  let fixture: ComponentFixture<OfferComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OfferComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OfferComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/offer/offer.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder, FormControl } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-offer',
+  templateUrl: './offer.component.html',
+  styleUrls: ['./offer.component.scss'],
+})
+export class OfferComponent implements OnInit {
+  public offerForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.makeForm();
+  }
+
+  makeForm() {
+    this.offerForm = new FormGroup({});
+  }
+
+  // Getters for template
+  get source() { return this.offerForm.get('source'); }
+  get selling() { return this.offerForm.get('selling'); }
+  get buying() { return this.offerForm.get('buying'); }
+  get amount() { return this.offerForm.get('amount'); }
+  get price() { return this.offerForm.get('price'); }
+
+  resetForm() {
+    this.offerForm.reset({ selling: { asset_type: 'native' }, buying: { asset_type: 'native' } });
+  }
+}
