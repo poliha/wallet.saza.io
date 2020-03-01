6575f3961ab0aad5839a38268d1d439b61e22536
oliha <poliha2002@gmail.com>
2020-02-29 22:41:37 -0500

    remove memo component reference

--- src/app/pages/build-tx/build-tx.module.ts
+++ src/app/pages/build-tx/build-tx.module.ts
@@ -9,7 +9,6 @@ import { BuildTxPage } from './build-tx.page';
 import { DynamicInputComponentModule } from 'src/app/components/dynamic-input/dynamic-input.module';
 import { SelectSourceComponentModule } from 'src/app/components/select-source/select-source.module';
 import { TxService, StellarService } from 'src/app/providers/providers';
-import { MemoComponentModule } from 'src/app/components/memo/memo.module';
 import { HeaderComponentModule } from 'src/app/components/header/header.module';
 import { MaterialModule } from 'src/app/material.module';
 import { SazaDateTimeComponentModule } from 'src/app/components/saza-date-time/saza-date-time.module';
@@ -30,7 +29,6 @@ const routes: Routes = [
     ReactiveFormsModule,
     DynamicInputComponentModule,
     SelectSourceComponentModule,
-    MemoComponentModule,
     HeaderComponentModule,
     MaterialModule,
     SazaDateTimeComponentModule,
