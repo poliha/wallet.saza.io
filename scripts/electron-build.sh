a5cfaece0e2c888b8349d4e3217503aacdb1785c
oliha <poliha2002@gmail.com>
2020-04-21 01:21:06 -0400

    use npm scripts

--- scripts/electron-build.sh
+++ scripts/electron-build.sh
@@ -12,7 +12,7 @@ mkdir dist
 
 
 echo "Building production release ..."
-node_modules/@angular/cli/bin/ng build --prod --base-href ./
+npm run build:prod
 
 echo "Making electron build ..."
-node_modules/@electron-forge/cli/dist/electron-forge.js make
+npm run electron:make
