a77a84087ee3c97a38a3c03b5f4e89a922a08ccb
oliha <poliha2002@gmail.com>
2020-04-26 12:57:50 -0400

    fix failing tests

--- src/app/pages/privacy-policy/privacy-policy.page.spec.ts
+++ src/app/pages/privacy-policy/privacy-policy.page.spec.ts
@@ -2,15 +2,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { PrivacyPolicyPage } from './privacy-policy.page';
+import { UserService } from 'src/app/providers/providers';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('PrivacyPolicyPage', () => {
   let component: PrivacyPolicyPage;
   let fixture: ComponentFixture<PrivacyPolicyPage>;
+  let userServiceSpy;
 
   beforeEach(async(() => {
+    userServiceSpy = jasmine.createSpyObj('UserService', ['isAuthValid']);
+    userServiceSpy.isAuthValid.and.returnValue(Promise.resolve(true));
     TestBed.configureTestingModule({
       declarations: [PrivacyPolicyPage],
-      imports: [IonicModule.forRoot()],
+      imports: [RouterTestingModule, IonicModule],
+      providers: [{ provide: UserService, useValue: userServiceSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PrivacyPolicyPage);
--- src/app/pages/usage-terms/usage-terms.page.spec.ts
+++ src/app/pages/usage-terms/usage-terms.page.spec.ts
@@ -2,15 +2,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { UsageTermsPage } from './usage-terms.page';
+import { UserService } from 'src/app/providers/providers';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('UsageTermsPage', () => {
   let component: UsageTermsPage;
   let fixture: ComponentFixture<UsageTermsPage>;
+  let userServiceSpy;
 
   beforeEach(async(() => {
+    userServiceSpy = jasmine.createSpyObj('UserService', ['isAuthValid']);
+    userServiceSpy.isAuthValid.and.returnValue(Promise.resolve(true));
     TestBed.configureTestingModule({
-      declarations: [ UsageTermsPage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [UsageTermsPage],
+      imports: [RouterTestingModule, IonicModule],
+      providers: [{ provide: UserService, useValue: userServiceSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UsageTermsPage);
